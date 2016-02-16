6937eb96e22f57306c72226155a3a8f745a9bddf
Ruoyan Qin <rq25@cornell.edu>
2016-02-16 09:15:50 -0500

    Closed Spring 2016 round.

--- newstudent/urls.py
+++ newstudent/urls.py
@@ -3,13 +3,7 @@ from .views import *
 
 urlpatterns = [
     # The main page for category
-    url(r'^$', basic, name='newstudent'),
+    # url(r'^$', basic, name='newstudent'),
     # When closed, replace above line with code below
-    # url(r'^$', newstudent_closed)
-   
-    # The main page for each individual category page
-    # url(r'^academic/', academic),
-    # url(r'^extracurricular/', extracurricular),
-    # url(r'^career/', career),
-    # url(r'^shortanswer/', shortanswer),
+    url(r'^$', newstudent_closed, name='newstudent')
 ]
--- selection/urls.py
+++ selection/urls.py
@@ -3,8 +3,9 @@ from .views import *
 
 urlpatterns = [
     # The main page for category
-    url(r'^$', login, name='upperclassman_login'),
+    # url(r'^$', login, name='upperclassman_login'),
+    
     # closed
-    # url(r'^$', closed),
-    url(r'^list/', selection, name='selection'),
+    url(r'^$', closed, name='upperclassman_login'),
+    # url(r'^list/', selection, name='selection'),
 ]
