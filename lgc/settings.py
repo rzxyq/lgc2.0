8adcd2f913aa9ee166c193d8dac8e3adb2357cab
Kim Zhao <zz86@KimdeMacBook-Pro.local>
2017-09-21 00:23:12 -0400

    hide password

--- lgc/settings.py
+++ lgc/settings.py
@@ -109,7 +109,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'app20377698@heroku.com'
-EMAIL_HOST_PASSWORD = 'ZHAOzhikun33'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
